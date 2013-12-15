12a2922b78f6086b87c7ab5e08468b61c0fe03be
Andy Lester <andy@petdance.com>
2013-12-15 10:52:03 -0600

    benchmark tool

---
+++ bench.pl
@@ -0,0 +1,17 @@
+#!?usr/bin/perl
+
+use strict;
+use warnings;
+
+use Benchmark ( ':hireswallclock', 'cmpthese' );
+
+my $count = 10000000;
+
+my %hash = ( x => 1, y => 1 );
+
+my $path = '/usr/local/bin/whatever';
+
+cmpthese($count, {
+        hashed => sub { return !exists $hash{$path} },
+        ored   => sub { return $path ne '.git' && $path ne '.svn' },
+    } );
