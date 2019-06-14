62f5d5732be74443f3d65a1f17f01d75412cf96e
Andy Lester <andy@petdance.com>
2019-06-14 13:44:40 -0500

    playing with benchmark

--- dev/bench.pl
+++ dev/bench.pl
@@ -2,16 +2,49 @@
 
 use strict;
 use warnings;
+use 5.010;
 
 use Benchmark ( ':hireswallclock', 'cmpthese' );
+use File::Next;
 
-my $count = 10000000;
+my $count = 1;
 
 my %hash = ( x => 1, y => 1 );
 
 my $path = '/usr/local/bin/whatever';
 
-cmpthese($count, {
-        hashed => sub { return !exists $hash{$path} },
-        ored   => sub { return $path ne '.git' && $path ne '.svn' },
+my $start = '/home/andy/src';
+
+cmpthese($count,
+    {
+        readdir => \&via_readdir,
+        glob    => \&via_glob,
+        iter    => \&via_iterator,
     } );
+
+
+
+sub via_readdir {
+    opendir( my $dh, $start ) or die;
+    my @foo = readdir($dh);
+
+    say 'readdir ', scalar @foo;
+}
+
+
+sub via_glob {
+    my @foo = glob( "$start/*" );
+
+    say 'glob ', scalar @foo;
+}
+
+
+sub via_iterator {
+    my $iter = File::Next::files( $start );
+    my @foo;
+    while ( my $file = $iter->() ) {
+        push( @foo, $file );
+    }
+
+    say 'iterator ', scalar @foo;
+}
